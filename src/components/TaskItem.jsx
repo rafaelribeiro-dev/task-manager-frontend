101e5113a71b4d2712359ead990c630eb3528ca5
Rafael Ribeiro <rafaelr.alves@yahoo.com.br>
2024-05-24 06:03:34 -0300

    feat: update and stiling TaskItem component

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,10 +1,31 @@
+import { AiFillDelete } from "react-icons/ai";
+
 const TaskItem = ({ task }) => {
     return (
-        <>
-            <p>{task.id}</p>
-            <h1>{task.description}</h1>
-            <p>{task.isCompleted ? "Tarefa Feita" : "Falta Concluir"}</p>
-        </>
+        <div className="task-item-container">
+            <div className="task-description">
+                <label
+                    classsName={
+                        task.iscompleted
+                            ? "checkbox-container-completed"
+                            : "checkbox-container"
+                    }
+                >
+                    {task.decription}
+                    <input type="checkbox" defaultChecked={task.iscompleted} />
+                    <span
+                        className={
+                            task.isCompleted
+                                ? "checkmark completed"
+                                : "checkmark"
+                        }
+                    ></span>
+                </label>
+            </div>
+            <div className="deldete">
+                <AiFillDelete size={20} color="#f97474" />
+            </div>
+        </div>
     );
 };
 
